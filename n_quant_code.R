78388444705bbb5591a0b9fe38cfd6659bd06bb8
mjthreesixfive <jones1.matthew@gmail.com>
2016-07-22 14:23:48 +0100

    Update n_quant_code.R
    Added some comments to explain what each chunk does.

--- n_quant_code.R
+++ n_quant_code.R
@@ -1,22 +1,23 @@
+#Set the directory containing cropped tif files (each image should contain just one nucleus).
 setwd("~/Desktop/160701_tif_crop")
+
+#get the file list
 files = list.files(getwd())
-files
+
+#get just the tissue sample IDs
 glands = paste(sapply(strsplit(files, split = '-'),'[[',4),
 sapply(strsplit(files, split = '-'),'[[',9))
 unique(glands)
 
+#in this case the result was:
 #5 68E1, 3 mdelta 1
-setwd("~/Desktop/160701_tif_crop")
-files = list.files(getwd())
-files
-glands = paste(sapply(strsplit(files, split = '-'),'[[',4),
-               sapply(strsplit(files, split = '-'),'[[',9))
-unique(glands)
 
+#do a similary analysis but counting the nuclei
 nuclei = paste(sapply(strsplit(files, split = '-'),'[[',4),
                sapply(strsplit(files, split = '-'),'[[',9),
                substr(sapply(strsplit(files, split = '-'),'[[',10),1,3),
                substr(sapply(strsplit(files, split = '.tif_nuc'),'[[',2),2,2))
 
+#these lines count the unique number of nuclei
 length(grep('68E1',unique(nuclei)))
 length(grep('mdelta',unique(nuclei)))
